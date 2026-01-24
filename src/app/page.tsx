df6883c6a09c2a6680afee173c46868dc97b8007
Haixin <haixinhuang502@gmail.com>
2026-01-23 22:44:18 -0800

    feat: changed home to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <h1>Home</h1>
+      <h1>Dashboard</h1>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </main>
   );
