77941e49ad5d617ba04b8ab6c895429f6bdf6a6f
Haixin <haixinhuang502@gmail.com>
2026-01-23 22:30:56 -0800

    feat: made functional top navbar and added new pages

---
+++ src/app/recipe/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+
+export default function Recipe() {
+  return (
+    <main>
+      <Navbar />
+      <h1>Recipe</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const navBarRight: React.CSSProperties = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "right",
+    gap: "70px",
+  };
+
+  const navBar: React.CSSProperties = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: "10px",
+    paddingRight: "70px",
+    paddingLeft: "20px",
+  };
+
+  return (
+    <nav style={navBar}>
+      {}
+      <Link href="/">
+        <Image src="/MTC_logo.png" alt="MTC logo" width={150} height={150} />
+      </Link>
+      <div style={navBarRight}>
+        <Link href="/">Dashboard</Link>
+        <Link href="/menuPlanning">Menu Planning</Link>
+        <Link href="/recipe">Recipe</Link>
+        <Link href="/permissions">Permissions</Link>
+      </div>
+    </nav>
+  );
 }
