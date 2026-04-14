d705de6572818e846dd4230acd5caf169a9b2925
derrickph33 <derrickph33@gmail.com>
2026-04-14 10:46:14 -0700

    fix: fixed build for clerk authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,6 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    // @ts-expect-error
     <ClerkProvider>
       <html lang="en" className="h-full">
         <body className="flex flex-col h-full bg-light-gray">
